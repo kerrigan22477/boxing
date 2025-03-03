7634b1af0def1ad2c9784d6d1927d0444b79c8c7
kerrigan22477 <k3rr16an@gmail.com>
2025-03-03 11:19:04 -0800

    fixed timer to stop displaying after it's finished and adjusted timing for the square's first appearance

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // start time is 5 seconds from now (technically 7 for lag)
-var startTime = new Date().getTime() + 7000;
+var startTime = new Date().getTime() + 6500;
 const dynamicElement = document.createElement('h1');
 
 // count down
@@ -12,21 +12,26 @@ var x = setInterval(function() {
 
     // time is in milliseconds so 20,000 = 20 seconds
     if (distance < 0) {
+        dynamicElement.remove();
         clearInterval(x);
+    } 
+    else if (distance >= 0 && distance <= 1000){
         dynamicElement.textContent = "Start!";
-    } else {
+        const container = document.getElementById("countdown");
+        container.appendChild(dynamicElement);
+    } 
+    else {
         dynamicElement.textContent = seconds;
+        const container = document.getElementById("countdown");
+        container.appendChild(dynamicElement);
     }
 
-    const container = document.getElementById("countdown");
-    container.appendChild(dynamicElement);
-
     // run every 1000 ms = 1 sec
-}, 1000);
+}, 500);
 
 
 // display square after count down
-const myTimeout = setTimeout(displaySquare, 8000);
+const myTimeout = setTimeout(displaySquare, 6500);
 
 function displaySquare() {
     document.getElementById("square").style.backgroundColor = "blue";
